f12acdd86511302a0c39a12cda1738e10dfd4d5b
aledmana <aledmana@hanmail.net>
2021-05-12 03:40:27 +0900

    1.0 Firebase Setting Updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7UrJKelxaDRvgYF-OhNavGXDFTtGkPew",
-    authDomain: "wcj-twitter.firebaseapp.com",
-    databaseURL: "https://wcj-twitter.firebaseio.com",
-    projectId: "wcj-twitter",
-    storageBucket: "wcj-twitter.appspot.com",
-    messagingSenderId: "386586579637",
-    appId: "1:386586579637:web:8cf0d02683ba8e9b7e64fc",
-    measurementId: "G-RHLF2S4DKW"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_API_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
